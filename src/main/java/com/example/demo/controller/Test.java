c00091013076e57774bbb4e11d7cddeb36c94144
unknown <eya_khechine@hotmail.com>
2023-10-27 19:47:23 +0100

    display reclam reponse

--- src/main/java/com/example/demo/controller/Test.java
+++ src/main/java/com/example/demo/controller/Test.java
@@ -97,6 +97,37 @@ public class Test {
     }
 
 
+    @GetMapping("/reponse")
+    public String getResponse() {
+
+        String qexec = "PREFIX ns: <http://www.semanticweb.org/user/ontologies/2023/9/TrocAPP-14#>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "\n" +
+                "SELECT ?reponse\n" +
+                "WHERE {\n" +
+                "?reponse rdf:type ns:Reponse .\n" +
+                "}";
+
+        Model model = JenaEngine.readModel("data/sem.owl");
+
+        QueryExecution qe = QueryExecutionFactory.create(qexec, model);
+        ResultSet results = qe.execSelect();
+
+        // write to a ByteArrayOutputStream
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        ResultSetFormatter.outputAsJSON(outputStream, results);
+
+        // and turn that into a String
+        String json = new String(outputStream.toByteArray());
+
+        JSONObject j = new JSONObject(json);
+        System.out.println(j.getJSONObject("results").getJSONArray("bindings"));
+
+        JSONArray res = j.getJSONObject("results").getJSONArray("bindings");
+
+        return j.getJSONObject("results").getJSONArray("bindings").toString();
+    }
 
 
 
